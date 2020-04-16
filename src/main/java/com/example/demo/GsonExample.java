00faaa83a0f6923ee2b4c42324f2fc6725f19b76
Johan Navarro <21sebas12@gmail.com>
2020-04-16 00:27:47 -0500

    Se crea la clase WebApplication donde se sobre escribe el metodo que permite indicar a Spring como va ha serializar, se crea una clase para Serializar y Desarializar el UserAuth retornado como resultado un objeto de strings

---
+++ src/main/java/com/example/demo/GsonExample.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import com.example.demo.user.domain.UserAuthRequest;
+import com.example.demo.user.domain.UserName;
+import com.example.demo.user.serialization.UserNameAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonExample {
+
+    public static void main(String[] args) {
+        // La idea es solo ver esta linea una sola vez.
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(UserName.class, new UserNameAdapter())
+                .create();
+        UserName userName = UserName.of("test-username");
+        String password = "password...";
+        UserAuthRequest request = UserAuthRequest.from(
+                userName,
+                password
+        );
+
+        System.out.println(gson.toJson(request));
+    }
+}
