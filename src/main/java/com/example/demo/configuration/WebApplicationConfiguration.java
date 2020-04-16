c772efd3e22de37c91e496709e0dd31f93bfa82b
Johan Navarro <21sebas12@gmail.com>
2020-04-16 09:08:47 -0500

    se configura el deserializador

--- src/main/java/com/example/demo/configuration/WebApplicationConfiguration.java
+++ src/main/java/com/example/demo/configuration/WebApplicationConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.demo.configuration;
 
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
@@ -8,12 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor // con esto colocamos el contructor.
 public class WebApplicationConfiguration implements WebMvcConfigurer {
+
+    private final Gson gson;
 
     // Metodo encargado de indicarle a Spring como convertir lo mensajes
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
+        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter(gson);
         converters.add(gsonHttpMessageConverter);
     }
 }
