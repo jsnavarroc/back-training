53e4866d2048f7e84b22c29d241eb50793974368
Johan Navarro <21sebas12@gmail.com>
2020-04-15 19:05:12 -0500

    se establece diferentes perfiles los cuales estan prod y dev, se hace un been que es para pruevas con una BD emebebida

--- src/main/java/com/example/demo/configuration/DatasourceConfiguration.java
+++ src/main/java/com/example/demo/configuration/DatasourceConfiguration.java
@@ -5,6 +5,9 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
@@ -12,6 +15,17 @@ import javax.sql.DataSource;
 public class DatasourceConfiguration {
 
     @Bean
+    @Profile({"test"})
+    public DataSource testDatasource(){
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript("classpath:scripts/schema.sql")
+            .addScript("classpath:scripts/data.sql")
+            .build();
+    }
+
+    @Bean
+    @Profile({"dev","prod"})
     public DataSource hikariDatasource(DatabaseCredentials credentials){
         HikariConfig config = new HikariConfig();
         String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s", credentials.getHost(), credentials.getPassword(), credentials.getDatabase() );
