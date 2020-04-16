0604ed32c840fc97a88964a885d7a1fd12f7a481
Johan Navarro <21sebas12@gmail.com>
2020-04-15 23:30:44 -0500

    se evidencia como cambia la clase al utilizar Lombok en UserName

--- src/main/java/com/example/demo/user/domain/UserName.java
+++ src/main/java/com/example/demo/user/domain/UserName.java
@@ -1,14 +1,13 @@
 package com.example.demo.user.domain;
 
 import com.example.demo.common.Preconditions;
+import lombok.Value;
 import org.apache.commons.lang3.StringUtils;
 
+@Value(staticConstructor = "of")
 public class UserName {
-    public  static  UserName of(String value){
-        return new UserName(value);
-    }
 
-    private final String value;
+    String value;
 
 
     private UserName(String value) {
@@ -16,17 +15,6 @@ public class UserName {
         Preconditions.checkAgument(StringUtils.isNotBlank(value));
         Preconditions.checkAgument(value.length() >= 6 );
         this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return "UserName{" +
-                "value='" + value + '\'' +
-                '}';
     }
 }
 
