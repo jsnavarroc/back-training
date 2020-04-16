375ae3da75faeac6e333816177f4627345fff93b
Johan Navarro <21sebas12@gmail.com>
2020-04-15 23:19:53 -0500

    en este punto se establece una clase comun que tiene las validaciones de los datos del sistema, llamado Precondition, esta clase retornará todos los errores que se den cuando no pasen los datos, por otro lado se instala la libreria COMMOSN LANG esta tiene diferentes metodos para la validación de datos.

--- src/main/java/com/example/demo/user/domain/UserName.java
+++ src/main/java/com/example/demo/user/domain/UserName.java
@@ -1,11 +1,20 @@
 package com.example.demo.user.domain;
 
+import com.example.demo.common.Preconditions;
+import org.apache.commons.lang3.StringUtils;
+
 public class UserName {
+    public  static  UserName of(String value){
+        return new UserName(value);
+    }
 
     private final String value;
 
 
     private UserName(String value) {
+        Preconditions.checkNotNull(value);
+        Preconditions.checkAgument(StringUtils.isNotBlank(value));
+        Preconditions.checkAgument(value.length() >= 6 );
         this.value = value;
     }
 
