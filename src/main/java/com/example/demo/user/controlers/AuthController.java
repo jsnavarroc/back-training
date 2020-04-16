3dd4dda419b8ed4a0a31388202192f674cdccc07
Johan Navarro <21sebas12@gmail.com>
2020-04-15 23:30:44 -0500

    se evidencia como cambia la clase al utilizar Lombok en UserName

--- src/main/java/com/example/demo/user/controlers/AuthController.java
+++ src/main/java/com/example/demo/user/controlers/AuthController.java
@@ -1,6 +1,9 @@
 package com.example.demo.user.controlers;
 
+import com.example.demo.user.domain.UserAuthRequest;
+import com.example.demo.user.domain.UserName;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,15 @@ public class AuthController {
     @GetMapping("/hello")
     public String greeting(){
         return "Hello world";
+    }
+
+    @PostMapping("/authenticate")
+    public UserAuthRequest autenticate(){
+        UserName userName = UserName.of("test-username");
+        String password = "password...";
+        return UserAuthRequest.from(
+                userName,
+                password
+        );
     }
 }
