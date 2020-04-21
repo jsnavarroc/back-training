fca47b72f19fe4d8af8ecb6096100486dd72cdb2
Johan Navarro <21sebas12@gmail.com>
2020-04-21 12:15:43 -0500

    aquí se tiene como ejemeplo una funcion de J8, en este puntoel cual espera un string y retorna un objeto Password

--- src/main/java/com/example/demo/user/serialization/GsonAdapter.java
+++ src/main/java/com/example/demo/user/serialization/GsonAdapter.java
@@ -1,7 +1,28 @@
 package com.example.demo.user.serialization;
 
+import com.example.demo.user.domain.Password;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonSerializer;
+import io.micrometer.core.instrument.FunctionTimer;
+
+import java.util.function.Function;
 
 public interface GsonAdapter<T> extends JsonDeserializer<T>, JsonSerializer<T> {
+    public  static <T> String toString (T object) {
+        return object.toString();
+    }
+
+    public static void main(String[] args) {
+        Function<String, Password>  creator = new Function<String, Password>(){
+            @Override
+            public Password apply(String s) {
+                return Password.of(s);
+            }
+        };
+
+
+        Password password = creator.apply("Password");
+
+        System.out.println(password);
+    }
 }
