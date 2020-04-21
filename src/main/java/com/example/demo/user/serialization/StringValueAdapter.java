88d39e3a263100d0ad534c39701e822c721dc366
Johan Navarro <21sebas12@gmail.com>
2020-04-21 12:07:14 -0500

    se implmentan el serilizer de la interfaz GsonAdapter, pero se tiene un porblema con el deserializer

--- src/main/java/com/example/demo/user/serialization/StringValueAdapter.java
+++ src/main/java/com/example/demo/user/serialization/StringValueAdapter.java
@@ -1,4 +1,19 @@
 package com.example.demo.user.serialization;
 
-public class StringValueAdapter {
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+public class StringValueAdapter<T extends StringSerializable> implements GsonAdapter<T> {
+    @Override
+    public T deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        String value = jsonElement.getAsString();
+        return null;
+    }
+
+    @Override
+    public JsonElement serialize(T t, Type type, JsonSerializationContext jsonSerializationContext) {
+        String value = t.valueOf();
+        return new JsonPrimitive(value);
+    }
 }
