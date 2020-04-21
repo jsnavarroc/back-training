6f8e690f2d92df78d768a7b55982001bb951611b
Johan Navarro <21sebas12@gmail.com>
2020-04-21 12:41:50 -0500

    se establece la configuracion de GSON para el Password, el cual se utiliza la funcion de JAVA8 sin refectorizar, sin embargo ya se refactoriza

--- src/main/java/com/example/demo/configuration/GsonConfiguration.java
+++ src/main/java/com/example/demo/configuration/GsonConfiguration.java
@@ -1,11 +1,16 @@
 package com.example.demo.configuration;
 
+import com.example.demo.user.domain.Password;
 import com.example.demo.user.domain.UserName;
+import com.example.demo.user.serialization.StringSerializable;
+import com.example.demo.user.serialization.StringValueAdapter;
 import com.example.demo.user.serialization.UserNameAdapter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
 
 @Configuration
 public class GsonConfiguration {
@@ -13,8 +18,15 @@ public class GsonConfiguration {
     // La idea es solo ver esta linea una sola vez.
     @Bean
     public Gson gson(){
+        Function<String, Password> creator = new Function<String, Password>(){
+            @Override
+            public Password apply(String s) {
+                return Password.of(s);
+            }
+        };
         return new GsonBuilder()
                 .registerTypeAdapter(UserName.class, new UserNameAdapter())
+                .registerTypeAdapter(Password.class, new StringValueAdapter<Password>(creator))
                 .create();
     }
 }
