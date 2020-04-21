52d9f35abb9f7bbdc94740b55e20587a659f7cdd
Johan Navarro <21sebas12@gmail.com>
2020-04-21 12:46:21 -0500

    se cambia por una funcion lambda bace

--- src/main/java/com/example/demo/configuration/GsonConfiguration.java
+++ src/main/java/com/example/demo/configuration/GsonConfiguration.java
@@ -18,7 +18,7 @@ public class GsonConfiguration {
     // La idea es solo ver esta linea una sola vez.
     @Bean
     public Gson gson(){
-        Function<String, Password> creator = s -> Password.of(s);
+        Function<String, Password> creator = Password::of;
         return new GsonBuilder()
                 .registerTypeAdapter(UserName.class, new UserNameAdapter())
                 .registerTypeAdapter(Password.class, new StringValueAdapter<Password>(creator))
