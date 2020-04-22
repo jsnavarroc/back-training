edbdb08669d59b420711f47f3caeb14a6067a38c
Johan Navarro <21sebas12@gmail.com>
2020-04-22 09:52:04 -0500

    se utiliza el Qualifier para indicarle al servicio que lea los metodos que se encuentran ahí

--- src/main/java/com/example/demo/repositories/InMemoryUserRepository.java
+++ src/main/java/com/example/demo/repositories/InMemoryUserRepository.java
@@ -3,10 +3,14 @@ package com.example.demo.repositories;
 import com.example.demo.user.domain.Password;
 import com.example.demo.user.domain.UserCreated;
 import com.example.demo.user.domain.UserName;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Repository
+@Qualifier("in-memory")
 public class InMemoryUserRepository  implements UserRepository{
     private final Map<Long, UserCreated> state = new HashMap<>();
 
