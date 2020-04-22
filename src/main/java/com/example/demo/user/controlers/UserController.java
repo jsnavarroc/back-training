30eaaca2b6a31f6005db704f993c4fab2e76117b
Johan Navarro <21sebas12@gmail.com>
2020-04-22 10:11:17 -0500

    se establece la implementacion de crear usuario en memoria

--- src/main/java/com/example/demo/user/controlers/UserController.java
+++ src/main/java/com/example/demo/user/controlers/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.user.controlers;
 
 import com.example.demo.user.domain.CreateUserRequest;
+import com.example.demo.user.services.UserServices;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+    private final UserServices services;
+
     @PostMapping
     public String createUser(@RequestBody CreateUserRequest userBody) {
         return userBody.toString();
--- src/main/java/com/example/demo/user/services/UserServices.java
+++ src/main/java/com/example/demo/user/services/UserServices.java
@@ -1,15 +1,23 @@
 package com.example.demo.user.services;
 
 import com.example.demo.common.Business;
+import com.example.demo.repositories.UserRepository;
 import com.example.demo.user.domain.Password;
 import com.example.demo.user.domain.UserCreated;
 import com.example.demo.user.domain.UserName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 @Business
 public class UserServices {
+    private  final  UserRepository repository;
 
-    public UserCreated userCreated(UserName userName, Password password){
-        return null;
+    @Autowired
+    public UserServices(@Qualifier("in-memory") UserRepository repository){
+        this.repository = repository;
     }
 
+    public UserCreated userCreated(UserName userName, Password password){
+        return repository.createOne(userName, password);
+    }
 }
