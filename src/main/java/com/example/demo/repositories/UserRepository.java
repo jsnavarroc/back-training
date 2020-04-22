6cc8a4ec76b11e06b11f5de8ca3dbd66d0133b12
Johan Navarro <21sebas12@gmail.com>
2020-04-22 09:35:45 -0500

    Se crea un repositorio donde se almacena los datos del usuario, esto se hace por medio de una interfaz

---
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repositories;
+
+import com.example.demo.user.domain.Password;
+import com.example.demo.user.domain.UserCreated;
+import com.example.demo.user.domain.UserName;
+
+public interface UserRepository {
+    UserCreated createOne(UserName userName, Password password);
+    UserCreated findOne(UserName userName);
+}
--- src/main/java/com/example/demo/user/services/UserServices.java
+++ src/main/java/com/example/demo/user/services/UserServices.java
@@ -1,10 +1,15 @@
 package com.example.demo.user.services;
 
 import com.example.demo.common.Business;
+import com.example.demo.user.domain.Password;
+import com.example.demo.user.domain.UserCreated;
+import com.example.demo.user.domain.UserName;
 
 @Business
 public class UserServices {
 
-
+    public UserCreated userCreated(UserName userName, Password password){
+        return null;
+    }
 
 }
