98551ad18ae9e72c61a0f547f20757313f30b8a0
Johan Navarro <21sebas12@gmail.com>
2020-04-23 12:58:24 -0500

    Se tiene que implementar el metodo jdbcTemplate.queryForObjet(SQL, objects, rwm),

--- src/main/java/com/example/demo/user/repositories/SqlUserRepository.java
+++ src/main/java/com/example/demo/user/repositories/SqlUserRepository.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Repository
@@ -61,6 +63,23 @@ public class SqlUserRepository implements UserRepository{
 
     @Override
     public UserCreated findById(Long id) {
+        String SQL = "select ID, USERNAME, PASSWORD from USERS where ID = ?";
+        Object[] objects = {id};
+
+        RowMapper<UserCreated> rowMapper = new RowMapper<UserCreated>() {
+            @Override
+            public UserCreated mapRow(ResultSet resultSet, int i) throws SQLException {
+                long id = resultSet.getLong("ID");
+                UserName username = UserName.of(resultSet.getString("USERNAME"));
+                Password password = Password.of(resultSet.getString("PASSWORD"));
+                return UserCreated.of(username, password, id);
+            }
+        };
+        /* *
+        * con el metodo queryForObject solo obtendremos la primera linea que cuampla con la
+        * condicion SQL, si colocamos el metodo queryForList retornara todos
+        * */
+        jdbcTemplate.queryForObject(SQL, objects, rowMapper );
         return null;
     }
 }
