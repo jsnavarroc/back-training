6e67a521bd196768d6280fcf9c82f31b306d00c0
Johan Navarro <21sebas12@gmail.com>
2020-04-23 11:59:28 -0500

    se establece el PreparedStatementSetter para aumentar la enficiencia de proceso en almacenamiento de la BD

--- src/main/java/com/example/demo/user/repositories/SqlUserRepository.java
+++ src/main/java/com/example/demo/user/repositories/SqlUserRepository.java
@@ -6,7 +6,11 @@ import com.example.demo.user.domain.UserName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Repository
 @Qualifier("users-sql")
@@ -24,10 +28,17 @@ public class SqlUserRepository implements UserRepository{
     public UserCreated createOne(UserName userName, Password password) {
         String SQL = "INCERT INTO USER (USERNAME, PASSWORD) VALUES (?.?)";
 
+        PreparedStatementSetter pss = new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, userName.getValue());
+                preparedStatement.setString(2, password.getValue());
+            }
+        };
         jdbcTemplate.update(
-          SQL,
-          userName.getValue(),
-          password.getValue()
+                SQL,
+                pss
+
         );
         return null;
     }
