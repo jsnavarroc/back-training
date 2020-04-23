7382fea5da90bab6943983114eecda4a65abd120
Johan Navarro <21sebas12@gmail.com>
2020-04-23 10:53:45 -0500

    se establece la configuracion de  JdbcTemplate

--- src/main/java/com/example/demo/user/repositories/SqlUserRepository.java
+++ src/main/java/com/example/demo/user/repositories/SqlUserRepository.java
@@ -3,12 +3,23 @@ package com.example.demo.user.repositories;
 import com.example.demo.user.domain.Password;
 import com.example.demo.user.domain.UserCreated;
 import com.example.demo.user.domain.UserName;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Qualifier("users-sql")
 public class SqlUserRepository implements UserRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public SqlUserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
     @Override
     public UserCreated createOne(UserName userName, Password password) {
         return null;
