9832362b05bc2705023ec978eac8215c8ad72013
Johan Navarro <21sebas12@gmail.com>
2020-04-25 15:49:24 -0500

    se hace prueba de que el serializador generico si funcione

--- src/main/java/co/jsnvarroc/orders/Pruebas.java
+++ src/main/java/co/jsnvarroc/orders/Pruebas.java
@@ -1,11 +1,23 @@
 package co.jsnvarroc.orders;
 
+import co.jsnvarroc.orders.product.domain.BasePrice;
 import co.jsnvarroc.orders.product.domain.ProductId;
+import co.jsnvarroc.orders.product.domain.TaxRate;
+
+import java.math.BigDecimal;
 
 public class Pruebas {
     public static void main(String[] args) {
         Long value = Long.valueOf(5);
         ProductId productId = ProductId.of(value);
         System.out.println(productId);
+
+        String value2 = "0.1";
+        BasePrice basePrice = BasePrice.of(new BigDecimal(value2));
+        System.out.println(basePrice);
+
+        String value3 = "0.2";
+        TaxRate taxRate = TaxRate.of(new BigDecimal(value3));
+        System.out.println(taxRate);
     }
 }
--- src/main/java/co/jsnvarroc/orders/product/controllers/ProductController.java
+++ src/main/java/co/jsnvarroc/orders/product/controllers/ProductController.java
@@ -1,18 +1,21 @@
 package co.jsnvarroc.orders.product.controllers;
 
+import co.jsnvarroc.orders.product.domain.BasePrice;
 import co.jsnvarroc.orders.product.domain.ProductId;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
 
-    @GetMapping("/{id}")
-    public ProductId getDayHour(@PathVariable Long id) {
-        ProductId productId = ProductId.of(id);
-        return productId;
+    @GetMapping("/{value}")
+    public BasePrice getDayHour(@PathVariable String value) {
+        BasePrice basePrice = BasePrice.of(new BigDecimal(value));
+        return basePrice;
     }
 }
