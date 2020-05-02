1298310e3b606574da2aa9db9fb0a09a21402f80
Johan Navarro <21sebas12@gmail.com>
2020-05-02 18:12:35 -0500

    Se corrige la forma de cargar las anotiaciones de MockMvc

--- src/test/java/co/jsnvarroc/orders/user/controlers/UserControllerTest.java
+++ src/test/java/co/jsnvarroc/orders/user/controlers/UserControllerTest.java
@@ -7,7 +7,9 @@ import co.jsnvarroc.orders.user.services.UserServices;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -23,7 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(UserController.class)
+@SpringBootTest()
+@AutoConfigureMockMvc
 class UserControllerTest {
 
     @Autowired
