848cd071665162a3b69c56d95153c8b2812789a9
Johan Navarro <21sebas12@gmail.com>
2020-05-08 14:37:28 -0500

    TDD, hacemos que la prueba pase.

--- src/main/java/co/jsnvarroc/orders/user/controlers/CalculatorController.java
+++ src/main/java/co/jsnvarroc/orders/user/controlers/CalculatorController.java
@@ -1,4 +1,7 @@
 package co.jsnvarroc.orders.user.controlers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
 }
--- src/test/java/co/jsnvarroc/orders/user/controlers/CalculatorControllerTest.java
+++ src/test/java/co/jsnvarroc/orders/user/controlers/CalculatorControllerTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CalculatorControllerTest {
 
     @Autowired
