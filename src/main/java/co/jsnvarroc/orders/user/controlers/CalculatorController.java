12d1b7f633798e6a4fb84093d5464464b635f7ab
Johan Navarro <21sebas12@gmail.com>
2020-05-11 10:14:17 -0500

    CalcularotCotrller Test

--- src/main/java/co/jsnvarroc/orders/user/controlers/CalculatorController.java
+++ src/main/java/co/jsnvarroc/orders/user/controlers/CalculatorController.java
@@ -1,7 +1,15 @@
 package co.jsnvarroc.orders.user.controlers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/calculator")
 public class CalculatorController {
+
+    @GetMapping
+    public void calculator(){
+
+    }
 }
