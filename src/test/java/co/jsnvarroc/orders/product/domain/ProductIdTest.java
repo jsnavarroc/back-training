3c5b434fe1b3205e0f08bce7b610b09e4f547370
Johan Navarro <21sebas12@gmail.com>
2020-05-11 12:20:16 -0500

    Taller 5, se crea pruebas unitarias para el dominio ProductId

---
+++ src/test/java/co/jsnvarroc/orders/product/domain/ProductIdTest.java
@@ -0,0 +1,50 @@
+package co.jsnvarroc.orders.product.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductIdTest {
+
+    @Test
+    @DisplayName("No debería crear id de productos para casos inválidos")
+    void isShouldNotPass() {
+        //arrange
+        Long productId = null;
+        Long productId1 = 0L;
+        Long productId2 = -1L;
+
+        //assertions
+        assertAll(
+                () -> assertThrows(NullPointerException.class, () ->  ProductId.of(productId)),
+                () -> assertThrows(IllegalArgumentException.class, () ->  ProductId.of(productId1)),
+                () -> assertThrows(IllegalArgumentException.class, () ->  ProductId.of(productId2))
+        );
+
+    }
+
+    @Test
+    @DisplayName("Deberia crear Id de product validos")
+    void isShouldPass(){
+        //arrange
+        Long productId = 1L;
+        Long productId1 = 22L;
+        Long productId2 = 50L;
+
+        //act
+        Executable executable = () -> assertNotNull(ProductId.of(productId));
+
+        //assertions
+        assertAll(
+                executable,
+                () -> assertNotNull(ProductId.of(productId1)),
+                () -> assertNotNull(ProductId.of(productId2))
+        );
+
+
+    }
+
+}
